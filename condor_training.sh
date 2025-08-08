92672025bef3c5fb1249cfa72b08d4c639774fd5
Oguz Guzel <44399848+Oguz-Guzel@users.noreply.github.com>
2025-08-08 16:10:46 +0200

    Update condor training scripts to use train.py and include max_depth argument

--- condor_training.sh
+++ condor_training.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source /afs/cern.ch/work/a/aguzel/private/bamboo_105/pytorch_env/bin/activate
 cd /afs/cern.ch/work/a/aguzel/private/bamboo_105/HHtoWWbb_Run3/XGBoost
-python XGBoost.py --output_dir $1
+python train.py --output_dir $1 --max_depth $2
